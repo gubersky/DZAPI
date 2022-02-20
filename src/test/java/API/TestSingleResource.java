df5c672caa3c04e7b12bc843c82100add1a1c665
gubersky <https://github.com/gubersky/Burger.git>
2022-02-20 19:49:38 +0200

    Initial commit

---
+++ src/test/java/API/TestSingleResource.java
@@ -0,0 +1,33 @@
+package API;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.restassured.RestAssured.given;
+
+public class TestSingleResource {
+
+    private final static String URL = "https://reqres.in/";
+
+    @Test
+    public void responseSingleResource(){
+
+        String text = "To keep ReqRes free, contributions towards server costs are appreciated!";
+
+        ApiSpec.installSpec(ApiSpec.requestSpec(URL),
+                ApiSpec.responseSpec(200));
+
+       ResponseSingleResource responseSingleResource = given()
+                .when()
+                .get("api/unknown/2")
+                .then().log().all()
+                .extract().body().jsonPath()
+                       .getObject("support", ResponseSingleResource.class);
+       
+       Assert.assertEquals(responseSingleResource.getText(),text);
+
+    }
+}
